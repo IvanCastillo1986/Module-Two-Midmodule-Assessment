7c26887da7053b964d8d398df8dd8e245a2d7a0d
Ivan Castillo <total180training@gmail.com>
2021-02-25 11:17:23 -0500

    First commit. Completed most html, beginning of js

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let diceRoll = document.getElementById("dice-input")
+    let rollBtn = document.getElementById("dice-button")
+    let die = document.getElementById("dice-para")
+    let sum = document.getElementById("sum-para")
+
+    
+
+})
