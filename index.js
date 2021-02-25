1aa15ed7ce11202c61ea239856a5993398bcc54f
Ivan Castillo <total180training@gmail.com>
2021-02-25 16:52:25 -0500

    Second commit. Unfinished.

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
+
 document.addEventListener("DOMContentLoaded", () => {
 
-    let diceRoll = document.getElementById("dice-input")
+    let diceNum = document.getElementById("dice-input")
     let rollBtn = document.getElementById("dice-button")
-    let die = document.getElementById("dice-para")
-    let sum = document.getElementById("sum-para")
+    let dieDisplay = document.getElementById("dice-para")
+    let sumDisplay = document.getElementById("sum-para")
+
+
+    // This function takes button click and displays a specified dice from 1-6 and displays it in #dice-para 
+    rollBtn.addEventListener("click", () => {
+        function randomNum() {
+            let min = 0
+            let max = 6
+            return Math.floor(Math.random(min) * max)
+        }
+        
+        function diceRoll(num) {
+            let diceArr = ["&#9856", "&#9857", "&#9858", "&#9859", "&#9860", "&#9861"]
+            dieDisplay.innerHTML = diceArr[num]
+
+            sumDisplay.textContent = num
+        }
+        diceRoll(randomNum())
+
+
+        let ul = document.querySelector("ul")
+        let li = document.createElement("li")
+        li.textContent = 
+        ul.appendChild(li)
+
+    })
+
+    
 
     
 
 })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
